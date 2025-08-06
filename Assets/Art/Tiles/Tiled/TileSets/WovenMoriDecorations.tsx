2c29f2186b789120bbbe5bf6888374e5b06fce0a
Xander Adams <xanderadams555@gmail.com>
2025-08-05 23:32:32 -0500

    Fixing a broke

--- Assets/Art/Tiles/Tiled/TileSets/WovenMoriDecorations.tsx
+++ Assets/Art/Tiles/Tiled/TileSets/WovenMoriDecorations.tsx
@@ -2,23 +2,23 @@
 <tileset version="1.10" tiledversion="1.11.2" name="WovenMoriDecorations" tilewidth="16" tileheight="16" tilecount="12" columns="3">
  <image source="../../WovenMoriDecorations.png" width="48" height="64"/>
  <tile id="0">
-  <objectgroup draworder="index" id="4">
-   <object id="4" x="2" y="7" width="12" height="7"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.75" y="6.875" width="12.375" height="7.125"/>
   </objectgroup>
  </tile>
  <tile id="1">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="8" y="6" width="8" height="8"/>
+   <object id="1" x="7.875" y="6.0625" width="8.125" height="8.125"/>
   </objectgroup>
  </tile>
  <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="6" width="8" height="8"/>
+   <object id="1" x="-0.125" y="6" width="8.25" height="8.1875"/>
   </objectgroup>
  </tile>
  <tile id="3">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="7.04348" y="7.04348" width="9" height="8.95652"/>
+   <object id="1" x="6.9375" y="6.9375" width="9" height="9"/>
   </objectgroup>
  </tile>
  <tile id="4">
@@ -28,7 +28,7 @@
  </tile>
  <tile id="5">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="6.96875" width="9.03125" height="9.09375"/>
+   <object id="1" x="0.0625" y="6.9375" width="8.875" height="9.0625"/>
   </objectgroup>
  </tile>
  <tile id="6">
@@ -61,4 +61,18 @@
    <object id="1" x="0" y="0" width="10" height="14"/>
   </objectgroup>
  </tile>
+ <wangsets>
+  <wangset name="Decorations" type="mixed" tile="0">
+   <wangcolor name="Table" color="#ff0000" tile="7" probability="1"/>
+   <wangtile tileid="3" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="4" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="5" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="6" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="7" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="8" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="9" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="10" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="11" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
