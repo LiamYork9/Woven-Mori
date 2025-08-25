abcdeb3e1e74698ba4176b442a92d31a9133cad2
Liam York <yorkliam08@gmail.com>
2025-08-25 16:32:51 -0500

    More work on skills

---
+++ Assets/Scripts/PlayerScripts/SkillMaker.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using MoriSkills;
+
+public class SkillMaker : MonoBehaviour
+{
+    public List<Skills> madeSkills;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SkillCreation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void SkillCreation()
+    {
+        Skills EndBringer = new Skills(SkillId.EndBringer, "End Bringer", Element.Fate, Target.single, Category.Magic, 100, 100, 5, "This shit kills people", 0, 2);
+        madeSkills.Add(EndBringer);
+        Skills SmallPunch = new Skills(SkillId.SmallPunch, "Small Punch", Element.None, Target.single, Category.Physical, 10, 100, 1, "You punch them", 100, 1);
+        madeSkills.Add(SmallPunch);
+        Skills StaticShock = new Skills(SkillId.StaticShock, "Static Shock", Element.Air, Target.mutipleEnemy, Category.Magic, 40, 70, 4, "Hit mutiple guys with lighting", 0, 1);
+        madeSkills.Add(StaticShock);
+    }
+}
