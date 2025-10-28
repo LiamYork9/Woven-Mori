ade3a0fa95a65069ad537811ed669f351c6319d2
Xander Adams <xanderadams555@gmail.com>
2025-10-28 15:56:43 -0500

    Followers just work

--- Assets/Scripts/PlayerScripts/Follower.cs
+++ Assets/Scripts/PlayerScripts/Follower.cs
@@ -3,54 +3,44 @@ using UnityEngine;
 
 public class Follower: PlayerController
 {
-    public float moveSpeed = 5f;
-    public Transform movePoint;
-    public LayerMask stopsMovement;
 
     public GameObject following = null;
-    public Follower follower = null;
     
-
-    private float tempDist = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         movePoint.parent = null;
+        if (following == null)
+        {
+            Follow(GameObject.FindWithTag("Player").GetComponent<PlayerController>());
+        }
+        else
+        {
+            if (following.GetComponent<PlayerController>().follower != this)
+            {
+                Follow(following.GetComponent<PlayerController>());
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("Following");
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, movePoint.position) < .05f)
-        {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) >= 0.9f)
-            {
-                tempDist = Input.GetAxisRaw("Horizontal");
-                tempDist /= Math.Abs(tempDist);
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(tempDist, 0f, 0f), 0.2f, stopsMovement))
-                {
-                    Debug.Log(tempDist + " Horizontal");
-                    movePoint.position += new Vector3(tempDist, 0f, 0f);
-                }
-            }
-
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) >= 0.9f)
-            {
-                tempDist = Input.GetAxisRaw("Vertical");
-                tempDist /= Math.Abs(tempDist);
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, tempDist, 0f), 0.2f, stopsMovement))
-                {
-                    Debug.Log(tempDist + " Vertical");
-                    movePoint.position += new Vector3(0f, tempDist, 0f);
-                }
-            }
-        }
     }
-    
+
+    public void Move()
+    {
+        movePoint.position = following.transform.position;
+        if (follower != null)
+        {
+            follower.Move();
+        }
+    }    
     public void Follow(PlayerController target)
     {
-        
+        target.Follower(this);
     }
 }
